d44bb795a1553863337f33e9b5ae81395bc61b40
Tulani <tulanidepaula@gmail.com>
2025-04-04 21:38:59 -0300

    integração com api e correção do sass

--- src/Pages/LivrosDoados/LivrosDoados.jsx
+++ src/Pages/LivrosDoados/LivrosDoados.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
-import despertar from "../../assets/Despertar.png"; 
-import comida from "../../assets/Comida.png";
-import python from "../../assets/Python.png";
-import protagonista from "../../assets/Protagonista.png";
 import s from "./livrosdoados.module.scss"
+import axios from "axios"
 
 
 const LivrosDoados = () => {
+
+    const [livros, setLivros] = useState([])
+    const puxarLivros = async () => {
+        const resposta = await axios.get('https://api-livros-1jof.onrender.com/livros')
+        setLivros(resposta.data)
+    }
+    useEffect(() => {
+        puxarLivros();
+    }, []);
+
     return (
         <Carousel className={s.container}
             showArrows={true}
@@ -18,22 +25,19 @@ const LivrosDoados = () => {
             interval={3000}
             showThumbs={false}
         >
-            <figure className={s.boxImage}>
-                <img src={despertar} alt="Livro o Despertar da Senhorita Prim, capa azul florida e um poltrona na capa" title="Livro o Despertar da Senhorita Prim"/>
-                <figcaption className={s.legend}>O despertar da senhorita Prim - Natalia Sanmartin</figcaption>
-            </figure>
-            <figure className={s.boxImage}>
-                <img src={comida} alt="Livro Comida & Cozinha- capa branca com talheres sendo uma colher com tempero natural" title="Livro Comida & Cozinha"/>
-                <figcaption className={s.legend}>Comida & Cozinha - Harold McGee</figcaption>
-            </figure>
-            <figure className={s.boxImage}>
-                <img src={python} alt="Livro Python sem mistérios - capa azul e o símbolo da linguagem" title="Livro Python sem mistérios"/>
-                <figcaption className={s.legend}>Python sem Mistérios - Joel Saade</figcaption>
-            </figure>
-            <figure className={s.boxImage}>
-                <img src={protagonista} alt="Protagonista - Capa avermelhada com o nome na diagonal com letra branca" title="Protagonista" />
-                <figcaption className={s.legend}>O protagonista - Susane Andrade - Ficção</figcaption>
-            </figure>
+                {livros.map((item)=>(
+                    <section className={s.boxImage}>
+                        <section>
+                            <img src={item.image_url} alt={`Titulo do livro ${item.titulo}`} />
+                            <section className={s.Legend}>
+                                <h3>{item.titulo}</h3>
+                                <p>{item.autor}</p>
+                                <p>{item.categoria}</p>
+                            </section>
+                        </section>
+                    </section>
+                    ))
+                }
         </Carousel>
     );
 };
--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,28 +1,65 @@
 import livropDoar from "../../assets/livropDoar.png"
 import s from "./querodoar.module.scss"
-
+import { useState,useEffect } from "react"
+import axios from "axios"
 
 const QueroDoar = () => {
+
+    const [titulo, setTitulo]= useState("")
+    const [categoria, setCategoria]= useState("")
+    const [autor, setAutor]= useState("")
+    const [image_url, setImage_url]= useState("")
+
+    const capturaTitulo= (e)=>{
+        setTitulo(e.target.value)
+    }
+    const capturaCategoria= (e)=>{
+        setCategoria(e.target.value)
+    }
+    const capturaAutor= (e)=>{
+        setAutor(e.target.value)
+    }
+    const capturaImage_Url= (e)=>{
+        setImage_url(e.target.value)
+    }
+
+    const enviarDados = async() =>{
+        const dadosEnviar = {
+            titulo,
+            categoria,
+            autor,
+            image_url
+        }
+        console.log(dadosEnviar)
+        await axios.post("https://api-livros-1jof.onrender.com/livros",dadosEnviar)
+    }
+
+    const puxarLivros = async () => {
+        const resposta = await axios.get('https://api-livros-1jof.onrender.com/livros')
+        setLivros(resposta.data)
+    }
+    useEffect(()=>{
+        puxarLivros()
+    },[])
+
     return (
         <section className={s.container}>
             <p>
                 Por favor, preencha o formulário com suas informações e as informações do Livro
             </p>
-            <form action="">
+            <form onSubmit={(e)=>e.preventDefault()}>
                 <div>
                     <img src={livropDoar} alt="livro aberto com borda azul" />
                     <h2>Informações do Livro</h2>
                 </div>
-                <input type="text" name="" id="" placeholder="Título" />
-                <input type="text" name="" id="" placeholder="Categoria" />
-                <input type="text" name="" id="" placeholder="Autor" />
-                <input type="text" name="" id="" placeholder="Link da Imagem" />
-                <input type="submit" className={s.button} value="Doar" />
+                <input type="text" placeholder="Título" onChange={capturaTitulo} required />
+                <input type="text" placeholder="Categoria" onChange={capturaCategoria} required/>
+                <input type="text" placeholder="Autor" onChange={capturaAutor} required/>
+                <input type="text" placeholder="Link da Imagem" onChange={capturaImage_Url} required/>
+                <input type="submit" className={s.button} value="Doar" onClick={enviarDados} />
             </form>
         </section>
-
     )
-
 }
 
 export default QueroDoar
